cd30543473681d0e38f6fd226ff51aaf3c976e2c
Alex Sunity <san.suns.son@yandex.ru>
2024-09-03 17:50:16 +0700

    Added /docs dir for built project

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [ react() ],
   base: '/penrose-triangle/',
+  build: {
+    outDir: 'docs'
+  },
   resolve: {  
     alias: {  
       components: '/src/components',
